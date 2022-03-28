569ffe19c3cb760edf9f1586aa9777d822c6c4ba
Kija <kmboard1@yahoo.com>
2022-03-28 19:11:40 -0400

    finally

---
+++ Develop/assets/JS/script.js
@@ -0,0 +1,9 @@
+var currentTimeEl= $('currentTime');
+var currentDayEl= $('currentDay');
+var currentHourEl= $('currentHour');
+var userInput= $('userInput');
+
+
+
+var currentDay;
+$("#4a").text(time);
